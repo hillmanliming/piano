79af0e2bf651a12c17312d2b16bbdac5aa40fcad
LiMing Hillman <503055@student.fontys.nl>
2024-01-10 10:56:47 +0100

    send email using emailjs

--- assets/script/javascript.js
+++ assets/script/javascript.js
@@ -6,3 +6,25 @@ window.addEventListener("resize", () => {
     document.body.classList.remove("resize-animation-stopper");
   }, 400);
 });
+
+function sendMail(){
+ (function(){
+  emailjs.init(QzVx1feoLPuZn9-QX); //account public key
+ })();
+
+ var params = {
+  sendername: document.querySelector("#sendername").value,
+  subject: document.querySelector("#subject").value,
+  message: document.querySelector("#message").value
+
+ };
+
+ var serviceID = "service_i36re8v" //email service id
+ var templateID = "template_o7u169n"  // email template id
+
+ emailjs.send(serviceID, templateID, params)
+ .then ( res =>{
+  alert("Email sent successfully!")
+ })
+ .catch()
+}
