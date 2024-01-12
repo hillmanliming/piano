62b78a5a23fe41e6ac3275a88d0b3e561f61d445
LiMing Hillman <503055@student.fontys.nl>
2024-01-12 16:34:36 +0100

    character requirement and 'send' button on or off

--- assets/script/javascript.js
+++ assets/script/javascript.js
@@ -1,16 +1,22 @@
-let resizeTimer;
-window.addEventListener("resize", () => {
-  document.body.classList.add("resize-animation-stopper");
-  clearTimeout(resizeTimer);
-  resizeTimer = setTimeout(() => {
-    document.body.classList.remove("resize-animation-stopper");
-  }, 400);
-});
+
+// let resizeTimer;
+// window.addEventListener("resize", () => {
+//   document.body.classList.add("resize-animation-stopper");
+//   clearTimeout(resizeTimer);
+//   resizeTimer = setTimeout(() => {
+//     document.body.classList.remove("resize-animation-stopper");
+//   }, 400);
+// });
+
+
+let sendName = document.querySelector("#name");
+let subject = document.querySelector("#subject");
+let message = document.querySelector("#message");
+let submitButton = document.querySelector("#send-button");
 
 function sendMail(){
   let params = {
     name : document.getElementById("name").value,
-    email : document.getElementById("email").value,
     subject : document.getElementById("subject").value,
     message : document.getElementById("message").value,
     }
@@ -20,42 +26,84 @@ function sendMail(){
     )
 }
 
-let sendName = document.querySelector("#name");
-let subject = document.querySelector("#subject");
-let message = document.querySelector("#message");
-let submitButton = document.querySelector("#send-button");
+/**
+ * 
+ * Form
+ */
 
-subject.disabled = true; //setting button state to disabled
-message.disabled = true; 
-submitButton.disabled = true; 
+const refs = {
+  form: document.body.querySelector("[data-form]"),
+  inputs: [...document.body.querySelectorAll("[data-form-input]")],
+  submit: document.body.querySelector("[data-form-submit]")
+}
 
-sendName.addEventListener("change", stateHandle);
-subject.addEventListener("change", stateHandle2);
-message.addEventListener("change", stateHandle3);
-// (parseInt(sendName.value).length < 3)
-function stateHandle() {
-    if  (sendName.value === ""){
-        subject.disabled = true; //button remains DISabled
-    } else {
-        subject.disabled = false; //button is ENabled
+
+const validate = () => {
+  let isValid = true;
+
+  for (let index = 0; index < refs.inputs.length; index++) {
+    switch (refs.inputs[index].dataset.formInput) {
+      case 'name': {
+        if (refs.inputs[index].value.length < 2 || refs.inputs[index].value.length > 9) {
+          isValid = false;
+          refs.inputs[index].style.border = '2px solid red';
+        } else {
+          refs.inputs[index].style.border = '2px solid black';
+
+        }
+        break;
+      }
+      case 'subject': {
+        if (!refs.inputs[index].value) {
+          isValid = false;
+        }
+        break;
+      }
+      case 'message': {
+        if (!refs.inputs[index].value) {
+          isValid = false;
+        }
+        break;
+      }
     }
+  }
+
+  refs.submit.disabled = !isValid;
 }
 
-function stateHandle2() {
-  if (subject.value === "") {
-      message.disabled = true; //button remains disabled
-  } else {
-      message.disabled = false; //button is enabled
-      
+const handleChange = () => {
+  validate();
+}
+
+const handleSubmit = (event) => {
+  event.preventDefault();
+
+  for (let index = 0; index < refs.inputs.length; index++) {
+    console.log(refs.inputs[index].dataset.formInput, refs.inputs[index].value);
   }
 }
 
-function stateHandle3() {
-  if ((sendName.value === "") || (subject.value === "") || (message.value === "")) {
-      submitButton.disabled = true; //button remains disabled
-  } else {
-      submitButton.disabled = false; //button is enabled
-  }
+/**
+ * Events
+ */
+for (let index = 0; index < refs.inputs.length; index++) {
+  refs.inputs[index].addEventListener("keydown" || "keyup", handleChange);
 }
+
+refs.form.addEventListener("submit", handleSubmit)
+
+validate()
+
+
+
+
+
+
+
+let form = document.querySelector("form")
+
+
+
+
 
 
